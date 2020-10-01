9e8c91b7e55648a37a0b64ff55e4190f0ce068bb
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-09-30 20:53:47 -0700

    set up screen&navigation

---
+++ screens/PlacesListScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+
+
+
+import HeaderButton from "../components/HeaderButton";
+
+const PlacesListScreen = props => {
+  return (
+    <SafeAreaView>
+        <Text>PlacesListScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+PlacesListScreen.navigationOptions = navData => {
+  return{
+    headerTitle: 'All Places',
+    headerRight: <HeaderButtons HeaderButtonComponent={HeaderButton}>
+       <Item
+         title="Add Place"
+         iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
+         onPress={() => {navData.navigation.navigate('NewPlace')}}
+       />
+    </HeaderButtons>
+  }
+};
+
+
+
+
+
+export default PlacesListScreen;
