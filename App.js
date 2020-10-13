d9e0bb21097d90c55b3683aa966aab6e52fa4b91
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-10-13 11:18:15 -0700

    diving into SQLLite for permanent data storage

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import ReduxThunk from "redux-thunk";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
 import PlacesNavigator from './navigation/PlacesNavigator';
-import placesReducer from "./store/places-reducer";
+import placesReducer from './store/places-reducer';
+import { init } from './helpers/db.js';
 
+init()
+  .then(() => {
+    console.log('Initialized database');
+  })
+  .catch(err => {
+    console.log('Initializind db failed')
+    console.log(err)
+  });
 
 const RootReducer = combineReducers({
     places : placesReducer
