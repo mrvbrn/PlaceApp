d6e6684540b3b25c08a87b2aa1583427252ad840
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-10-14 11:27:11 -0700

    fetching data from local database

--- helpers/db.js
+++ helpers/db.js
@@ -23,12 +23,30 @@ export const init = () => {
 };
 
 
-export const insertPlace = (title, imageUri, address, lat, lng ) => {
+export const insertPlaces = (title, imageUri, address, lat, lng ) => {
   const promise = new Promise((resolve, reject) => {
     db.transaction((tx) => {
       tx.executeSql(
         `INSERT INTO places (title, imageUri, address, lat, lng) VALUES(?, ?, ?, ?, ?);`, 
         [title, imageUri, address, lat, lng],
+        (_, result) => {
+          resolve(result);
+        },
+        (_, err) => {
+          reject(err);
+        }
+      );
+    });
+  })
+  return promise;
+};
+
+export const fetchPlaces = (title, imageUri, address, lat, lng ) => {
+  const promise = new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        'SELECT * FROM places', 
+        [],
         (_, result) => {
           resolve(result);
         },
