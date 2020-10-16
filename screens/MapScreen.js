8119b9d78f7c22a81e713aba3431cbd9260bc56d
Merve Baran <mrvbrn@Merves-MacBook-Air.local>
2020-10-16 11:45:48 -0700

    making the mark location saveable

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+
+import Colors from "../constants/Colors";
 
 const MapScreen = props => {
   const [selectedLocation, setSelectedLocation] = useState();
@@ -25,6 +27,18 @@ const MapScreen = props => {
       longitude:selectedLocation.lng
     }
   };
+
+  const savePickedLocationHandler = useCallback(() => {
+    if(!selectedLocation){
+        return;
+    }
+    props.navigation.navigate('NewPlace', {pickedLocation:selectedLocation});
+  }, [selectedLocation])
+
+  useEffect(() => {
+    props.navigation.setParams({saveLocation:savePickedLocationHandler})
+  },[savePickedLocationHandler])
+
   return (
     <MapView region={mapRegion} style={styles.map} onPress={selectLocationHandler}>
       <Marker title="Picked Location" coordinate={markerCoordinate}></Marker>
@@ -32,9 +46,27 @@ const MapScreen = props => {
   );
 };
 
+MapScreen.navigationOptions = navData => {
+  const saveBtn = navData.navigation.getParam('saveLocation')
+  return{
+    headerRight:
+    <TouchableOpacity style={styles.headerButton} onPress={saveBtn}>
+      <Text style={styles.headerButtonText}>Save</Text>
+    </TouchableOpacity>
+
+  }
+}
+
 const styles = StyleSheet.create({
   map:{
     flex:1
+  },
+  headerButton:{
+    marginHorizontal:20
+  },
+  headerButtonText:{
+    fontSize:16,
+    color:Platform.OS === 'android' ? 'white' : Colors.primary
   }
 });
 
